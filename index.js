18bcd8c2294d3b8042764a4c3711221e059a4d84
maheshkareeya <maheshkareeya@gmail.com>
2018-06-10 10:47:42 -0700

    Add cool face API

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const cool = require('cool-ascii-faces')
 const path = require('path')
+const { mongoose } = require('./database');
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'pug')
+    .get('/', (req, res) => res.render('pages/index'))
+    .get('/cool', (req, res) => res.send(cool()))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
