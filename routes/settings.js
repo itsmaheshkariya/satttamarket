6ae941443f47366da61a59dd85c204937c3064a2
maheshkareeya <maheshkareeya@gmail.com>
2018-06-12 20:28:35 -0700

    make

--- routes/settings.js
+++ routes/settings.js
@@ -18,16 +18,17 @@ router.get('/Settings/edit/:id', function(req, res) {
 })
 router.get('/Settings', function(req, res) {
 
-    Settings.find({}, function(err, settings) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('pages/settings', {
-                title: 'SAT MAT RAT- Settings',
-                settings: settings
-            });
-        }
-    });
+    // Settings.find({}, function(err, settings) {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         res.render('pages/settings', {
+    //             title: 'SAT MAT RAT- Settings',
+    //             settings: settings
+    //         });
+    //     }
+    // });
+    res.send("ok");
 
 });
 router.post('/Settings', function(req, res) {
