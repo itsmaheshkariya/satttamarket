7d441fa8f7fdee2fe02d33eb1490e4fbaf8808f0
maheshkareeya <maheshkareeya@gmail.com>
2018-06-16 03:58:40 -0700

    Adding login logout to admin pannel

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database: 'mongodb://maheshkareeya:mahesh619619Key@ds237610.mlab.com:37610/qcom',
+    screte: 'yourscrete'
+}
--- index.js
+++ index.js
@@ -4,24 +4,57 @@ const path = require('path')
 const crypto = require('crypto')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
+const expressValidator = require('express-validator');
+const expressMessages = require('express-messages');
+const flash = require('connect-flash');
+const passport = require('passport');
+const session = require('express-session');
 const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const Grid = require('gridfs-stream');
 const multer = require('multer');
 const GridFsStorage = require('multer-gridfs-storage')
-const URI = 'mongodb://maheshkareeya:mahesh619619Key@ds237610.mlab.com:37610/qcom';
+const config = require('./config/database')
+
+require('./config/passport')(passport);
+
+
+
+
+
+
+
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        req.flash('danger', 'please login');
+        res.redirect('/login');
+    }
+}
+
+
+
+
+
+
+
+// const URI = 'mongodb://maheshkareeya:mahesh619619Key@ds237610.mlab.com:37610/qcom';
 // const URI = 'mongodb://localhost/qcom';
-mongoose.connect(URI)
+mongoose.connect(config.database)
     .then(db => console.log('Db is connected'));
 let gfs;
-const conn = mongoose.createConnection(URI);
+const conn = mongoose.createConnection(config.database);
+
+
 conn.once('open', () => {
     //Init stream
     gfs = Grid(conn.db, mongoose.mongo);
     gfs.collection('uploads');
 })
 const storage = new GridFsStorage({
-    url: URI,
+    url: config.database,
     file: (req, file) => {
         return new Promise((resolve, reject) => {
             crypto.randomBytes(16, (err, buf) => {
@@ -45,29 +78,84 @@ let Satta = require('./models/satta');
 let satta = require('./routes/satta');
 let Settings = require('./models/settings');
 let settings = require('./routes/settings');
+let Users = require('./models/user');
+let users = require('./routes/users');
 // let Upimg = require('./models/upimg');
 // let upimg = require('./routes/upimg');
 
 
-
+require('./config/passport');
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'pug')
     .use(methodOverride('_method'))
+    //Express session middleware
+    .use(session({
+        secret: 'keyboard cat',
+        resave: true,
+        saveUninitialized: true
+
+    }))
+
+//Express session
+
+.use(require('connect-flash')())
+    .use(function(req, res, next) {
+        res.locals.messages = require('express-messages')(req, res);
+        next();
+    })
+
+
+// Express Validator Middleware
+.use(expressValidator({
+        errorFormatter: function(param, msg, value) {
+            var namespace = param.split('.'),
+                root = namespace.shift(),
+                formParam = root;
+
+            while (namespace.length) {
+                formParam += '[' + namespace.shift() + ']';
+            }
+            return {
+                param: formParam,
+                msg: msg,
+                value: value
+            };
+        }
+    }))
+    //Passport Config
+
+
+
+
+//Passport middle ware 
+.use(passport.initialize())
+    .use(passport.session())
+
+
+.get('*', function(req, res, next) {
+    res.locals.user = req.user || null;
+    next();
+})
+
+
+
+
 
 
 .use(bodyParser.urlencoded({ extended: false }))
     .use(bodyParser.json())
     .use('/', satta)
     .use('/', settings)
-    // .use('/', upimg)
+    .use('/', users)
     .get('/Upimg', function(req, res) {
         gfs.files.find().toArray((err, files) => {
 
 
             if (!files || files.length === 0) {
                 res.render('pages/upimg', { files: false });
+
             } else {
                 files.map(file => {
                     if (file) {
@@ -79,7 +167,10 @@ express()
                     }
                 })
             }
+
             res.render('pages/upimg', { files: files });
+
+
 
 
 
@@ -90,6 +181,7 @@ express()
 
 .post('/Upimg/upload', upload.single('file'), (req, res) => {
         // res.json({ file: req.file });
+        req.flash('success', 'File Uploaded Successfully');
         res.redirect('/Upimg');
 
     })
@@ -145,6 +237,7 @@ express()
             if (err) {
                 return res.status(404).json({ err: err })
             } else {
+                req.flash('success', 'File Deleted Successfully');
                 res.redirect('/Upimg');
             }
 
