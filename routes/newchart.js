ef5ed490886b4db74c89c4bd9b7f03583ccd923a
Ratan <ratanhiwale65@gmail.com>
2018-07-11 00:07:02 +0530

    panel added successfully

--- routes/newchart.js
+++ routes/newchart.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 //Bring article model
 // let Satta = require('../models/satta');
-let Chart = require('../models/newchart');
+let NewChart = require('../models/newchart');
 let Satta = require('../models/satta');
 
 
@@ -41,24 +41,24 @@ router.get('/patti/chart/edit/:id', function(req, res) {
 
 
 router.post('/patti/chart/edit/:id', function(req, res) {
-    let chart = new Chart();
-    chart.unikey = req.params.id;
-    chart.name = req.body.name;
+    let newchart = new NewChart();
+    newchart.unikey = req.params.id;
+    newchart.name = req.body.name;
    
-    chart.l1 = req.body.l1;
-    chart.l2 = req.body.l2;
-    chart.l3 = req.body.l3;
-    chart.m1 = req.body.m1;
-    chart.m2 = req.body.m2;
-    chart.r1 = req.body.r1;
-    chart.r2 = req.body.r2;
-    chart.r3 = req.body.r3;
-    chart.time = req.body.time;
-    chart.time1 = req.body.time1;
-    chart.date = req.body.date;
+    newchart.l1 = req.body.l1;
+    newchart.l2 = req.body.l2;
+    newchart.l3 = req.body.l3;
+    newchart.m1 = req.body.m1;
+    newchart.m2 = req.body.m2;
+    newchart.r1 = req.body.r1;
+    newchart.r2 = req.body.r2;
+    newchart.r3 = req.body.r3;
+    newchart.time = req.body.time;
+    newchart.time1 = req.body.time1;
+    newchart.date = req.body.date;
 
 
-    chart.save(function(err) {
+    newchart.save(function(err) {
         if (err) {
             console.log(err);
         } else {
