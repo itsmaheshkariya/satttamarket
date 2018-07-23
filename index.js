35f9610b16dffa678461e334a75cc93b26ae019b
maheshkareeya <maheshkareeya@gmail.com>
2018-07-22 23:29:56 -0700

    ok got it

--- index.js
+++ index.js
@@ -395,30 +395,37 @@ express()
 
     })
 })
+.delete('/Upimg/images/:id', (req, res) => {
+  gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+    if (err) {
+      return res.status(404).json({ err: err });
+    }
 
+    res.redirect('/Upimg');
+  });
 
-.delete('/Upimg/images/:filename', (req, res) => {
-
-
-      gfs.remove({filename: req.params.filename},  (err, gridStore) => {
-          if (err){
-            res.status(404).json({err:err})
-          }
-          else {
-            {
-              res.redirect('/admin');
-            }
-          }
-
-      })
-
-
-
-
-    })
-
-
-
+// .delete('/Upimg/images/:id', (req, res) => {
+//
+//
+//       gfs.remove({id: req.params.id},  (err, gridStore) => {
+//           if (err){
+//             res.status(404).json({err:err})
+//           }
+//           else {
+//             {
+//               res.redirect('/admin');
+//             }
+//           }
+//
+//       })
+//
+//
+//
+//
+//     })
+//
+//
+//
 
 
 
